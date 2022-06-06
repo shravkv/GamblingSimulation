199e65ef57f141da75534d45ee73a9954bb1dd49
sravan <sravan.k.vodnala@gmail.com>
2022-06-06 16:49:32 +0530

    Added:Gambler started with  everyday with

---
+++ src/Uc1_Gambler.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Uc1_Gambler {
+
+    public static int everyDayStake = 100;
+    public static final int everyGameBet = 1;
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        System.out.println("Welcome to Gambler Simulation program");
+    }
+
+}
